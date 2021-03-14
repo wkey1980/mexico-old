5f385ea539eb17dce99a1c91c8f6de1f89876d30
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2021-03-14 19:18:08 +0000

    added front-page.php & updated the code

---
+++ front-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template for displaying front-page.php
+ * 
+ * Contains all content from opening #content div until closing #content div
+ * 
+ * @package mexico
+ */
+?>
+
+<?php get_header(); ?>
+
+    <main id="content" class="site-main">
+
+        <?php if ( have_posts() ) : ?>
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <article class="post">
+                    <?php the_title(); ?>
+                </article>
+
+            <?php endwhile ?>
+
+        <?php else : ?>
+
+            <article class="post error">
+                <h1 class="404">No posts found</h1>
+            </article>
+
+        <?php endif; ?>
+
+    </main><!-- /#content -->
+
+<?php get_footer(); ?>
