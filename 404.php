6aa4e1fed82a97bc2a94bcd6d1c07d339a6149f1
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2021-03-14 18:23:56 +0000

    Updated 404.php

--- 404.php
+++ 404.php
@@ -5,3 +5,12 @@
  * @package mexico
  */
 ?>
+
+
+<?php get_header(); ?>
+
+<div class="error-404 not-found">
+    <h1><?php esc_html_e( 'Error, No Content Found', 'mexico' ); ?></h1>
+</div>
+
+<?php get_footer(); ?>
