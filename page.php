248124a3b74753a814ad0d49679bebe65655fcf1
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2021-03-14 19:00:14 +0000

    update page.php

--- page.php
+++ page.php
@@ -5,3 +5,29 @@
  * @package mexico
  */
 ?>
+
+<?php get_header(); ?>
+
+    <main id="content" class="site-main">
+
+        <?php if ( have_posts() ) : ?>
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <article class="post">
+                    <?php the_title(); ?>
+                </article>
+
+            <?php endwhile ?>
+
+        <?php else : ?>
+
+            <article class="post error">
+                <h1 class="404">No posts found</h1>
+            </article>
+
+        <?php endif; ?>
+
+    </main><!-- /#content -->
+
+<?php get_footer(); ?>
