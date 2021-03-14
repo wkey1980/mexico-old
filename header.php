2898e06c30a8371b403094f050ea46d007ebbdca
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2021-03-14 18:29:51 +0000

    Update header.php

--- header.php
+++ header.php
@@ -5,3 +5,21 @@
  * @package mexico
  */
 ?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+    <!-- Skip to content -->
+    <a class="skip-link screen-reader-text" href="#content">
+        <?php esc_html_e( 'Skip to Content', 'mexico' ); ?>
+    </a>
